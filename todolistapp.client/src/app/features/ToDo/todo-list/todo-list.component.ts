b20433e34035c2b3adb431745e6b2a56538edcda
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-11-26 23:57:44 +0100

    feat logic for add new to do item

--- todolistapp.client/src/app/features/ToDo/todo-list/todo-list.component.ts
+++ todolistapp.client/src/app/features/ToDo/todo-list/todo-list.component.ts
@@ -3,6 +3,7 @@ import { ToDoItemService } from '../services/to-do-item.service';
 import { ToDoItem } from '../models/todoItem.model';
 import { Observable } from 'rxjs';
 import { Subscription } from 'rxjs';
+import { AddTodoItemRequest } from '../models/add-todoItem-request.model';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,11 +12,19 @@ import { Subscription } from 'rxjs';
 })
 export class TodoListComponent implements OnInit, OnDestroy {
 
+  // -------------------------------------
+  // starter logic for todo list component
   id: string | null = null;
 
   editableId: string | null = null;
 
   editedToDoItems: { [key: string]: ToDoItem } = {};
+
+  isAddingNew = false;
+
+  newToDoItemTitle = '';
+
+  newToDoItemContent = '';
 
   paramsSubscription?: Subscription;
 
@@ -23,6 +32,8 @@ export class TodoListComponent implements OnInit, OnDestroy {
 
   toDoItems$?: Observable<ToDoItem[]>;
 
+  //--------------------------------------
+  // constructor function that run when the component is created
   constructor(
     private toDoItemService: ToDoItemService,
     private toDoitemService: ToDoItemService,) {
@@ -33,6 +44,36 @@ export class TodoListComponent implements OnInit, OnDestroy {
     this.toDoItems$ = this.toDoItemService.getAllToDoItems();
   }
 
+  //-------------------------------------
+  // Add new todo item
+  toggleAddNew() {
+    this.isAddingNew = true;
+  }
+
+  cancelAddNew() {
+    this.isAddingNew = false;
+    this.newToDoItemTitle = '';
+    this.newToDoItemContent = '';
+  }
+
+  // Submit new todo item form event
+  onSubmitNewToDoItem() {
+    const newTodo: AddTodoItemRequest = {
+      title: this.newToDoItemTitle,
+      content: this.newToDoItemContent
+    };
+
+    this.toDoItemService.addToDoItem(newTodo)
+      .subscribe(() => {
+        this.toDoItems$ = this.toDoItemService.getAllToDoItems();
+        this.cancelAddNew();
+      }, error => {
+        console.error('Error while adding new item:', error);
+      });
+  }
+
+  // Edit and Update logic
+  //-------------------------------------
   // is in edit mode
   isInEditMode(id: string): boolean {
     return this.editableId === id;
@@ -58,7 +99,9 @@ export class TodoListComponent implements OnInit, OnDestroy {
   onCancel(): void {
     this.editableId = null;
   }
+  //-------------------------------------
 
+  //-------------------------------------
   // Delete on click event
   onDelete(id: string): void {
     this.toDoItemService.deleteToDoItem(id)
@@ -70,6 +113,7 @@ export class TodoListComponent implements OnInit, OnDestroy {
     });
   }
 
+  //-------------------------------------
   // destroy
   ngOnDestroy(): void {
     this.paramsSubscription?.unsubscribe();
