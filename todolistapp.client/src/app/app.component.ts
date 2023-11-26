30925248a290de55b173c8712c814f7a9a229db5
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-11-26 17:10:37 +0100

    fix

--- todolistapp.client/src/app/app.component.ts
+++ todolistapp.client/src/app/app.component.ts
@@ -1,37 +1,38 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+// interface WeatherForecast {
+//   date: string;
+//   temperatureC: number;
+//   temperatureF: number;
+//   summary: string;
+// }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
+export class AppComponent {
+// export class AppComponent implements OnInit {
+//   public forecasts: WeatherForecast[] = [];
 
-  constructor(private http: HttpClient) {}
+//   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.getForecasts();
-  }
+  // ngOnInit() {
+  //   this.getForecasts();
+  // }
 
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
+  // getForecasts() {
+  //   this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
+  //     (result) => {
+  //       this.forecasts = result;
+  //     },
+  //     (error) => {
+  //       console.error(error);
+  //     }
+  //   );
+  // }
 
   title = 'todolistapp.client';
 }
--- todolistapp.client/src/app/app.module.ts
+++ todolistapp.client/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { TodoListComponent } from './features/ToDo/todo-list/todo-list.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TodoListComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, AppRoutingModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
