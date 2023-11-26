da0ddaae4d5e302d4bfbc37b0cdcd243b470c2d5
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-11-26 22:30:02 +0100

    fix

--- todolistapp.client/src/app/features/ToDo/todo-list/todo-list.component.spec.ts
+++ todolistapp.client/src/app/features/ToDo/todo-list/todo-list.component.spec.ts
@@ -28,19 +28,4 @@ describe('TodoListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should retrieve todos from the server', () => {
-    const mockToDoItem = [
-      { title: 'Todo 1', content: 'Todo 1 content', createdAt: new Date(), updatedAt: new Date(), isCompleted: false },
-      { title: 'Todo 2', content: 'Todo 2 content', createdAt: new Date(), updatedAt: new Date(), isCompleted: false }
-    ];
-
-    component.ngOnInit();
-
-    const req = httpMock.expectOne('/todo-list');
-    expect(req.request.method).toEqual('GET');
-    req.flush(mockToDoItem);
-
-    expect(component.toDoItem.length).toEqual(2);
-  });
 });
